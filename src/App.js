da4bcdeea4284ec95520098f403748816fb73c32
Tusharshrivastav <ts3657@gmail.com>
2021-09-23 18:26:09 +0530

    added styles in data list and added navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Books from "./components/Books";
 import Create from "./components/Create";
+import Navbar from "./components/Navbar";
 function App() {
   return (
     <>
       <Router>
+        <Navbar />
         <Switch>
           <Route path="/" exact component={Books} />
           <Route path="/create" exact component={Create} />
--- src/components/Books.js
+++ src/components/Books.js
@@ -1,9 +1,32 @@
 import React from "react";
 import { useGetBooksQuery } from "../app/services/bookService";
 const Books = () => {
-  const res = useGetBooksQuery();
-  console.log(JSON.stringify(res.data));
-  return <div>books</div>;
+  const { data = [], isLoading, isError } = useGetBooksQuery();
+  if (isLoading)
+    return (
+      <div className="container">
+        <h1>Loading</h1>
+      </div>
+    );
+  return (
+    <div className="container">
+      {data.map((book) => (
+        <div className="list" key={book.id}>
+          <div className="list--name">
+            <span>name</span>
+            {book.name}
+          </div>
+          <div className="list--author">
+            <span>author</span>
+            {book.author}
+          </div>
+          <div className="list--button">
+            <button>Delete Book</button>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default Books;
